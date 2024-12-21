b169424c5bcf86c6d5ad5d3d56c28fd1ec2b6426
Вячеслав <@DESKTOP-5E97UAD>
2024-12-21 14:10:41 +0300

    Первый коммит

---
+++ main.js
@@ -0,0 +1,27 @@
+/* 
+let a=5;
+*/
+/* Объем параллелограмма
+
+let a=prompt("Введите длину",6);
+let b=prompt("Введите ширину",4);
+let c=prompt("Введите высоту",10);
+
+console.log(Number(a)*Number(b)*Number(c));
+*/
+
+
+
+ let a=prompt();
+
+ if(a%2==0){
+    console.log("Число А - четное");
+ }
+ else{
+    console.log("Нечетное");
+ }
+
+
+
+
+
